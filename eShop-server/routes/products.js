44c721bff2076051a4617370aae308cc0c4b1cf3
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2023-12-16 09:44:34 +0600

    single upload done

--- eShop-server/routes/products.js
+++ eShop-server/routes/products.js
@@ -2,18 +2,44 @@ const express = require("express");
 const router = express.Router();
 const { Product } = require("../models/products");
 const { Category } = require("../models/category");
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      const isValid = FILE_TYPE_MAP[file.mimetype];
+      let uploadError = new Error('invalid image type');
+
+      if (isValid) {
+          uploadError = null;
+      }
+      cb(uploadError, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    console.log(file.originalname)
+    const removeExtention=file.originalname.split('.');	
+      const fileName = removeExtention[0].split(' ').join('-');
+      const extension = FILE_TYPE_MAP[file.mimetype];
+      cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
 
 router.get("/", async (req, res) => {
   let filter = {};
-  
-  
+
   if (req.query.categories) {
-    filter = {category:req.query.categories.split(',')};
+    filter = { category: req.query.categories.split(",") };
   }
 
   try {
-    const productList = await Product.find(filter).populate('category');
+    const productList = await Product.find(filter).populate("category");
     res.status(200).json(productList);
   } catch (error) {
     res.status(500).json({
@@ -21,7 +47,7 @@ router.get("/", async (req, res) => {
       message: error.message,
     });
   }
-})
+});
 
 router.get("/:id", async (req, res) => {
   try {
@@ -45,10 +71,11 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", uploadOptions.single("image"), async (req, res) => {
   try {
     const categoryId = req.body.category;
-
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
     const category = await Category.findById(categoryId);
     if (!category) {
       return res.status(404).json({
@@ -56,12 +83,13 @@ router.post("/", async (req, res) => {
         message: "Invalid category",
       });
     }
-
+    const fileName = file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     const newProduct = new Product({
       name: req.body.name,
       description: req.body.description,
       richDescription: req.body.richDescription,
-      image: req.body.image,
+      image: `${basePath}${fileName}`,
       images: req.body.images,
       brand: req.body.brand,
       price: req.body.price,
@@ -89,12 +117,12 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-if(!mongoose.isValidObjectId(req.params.id)){
-  return res.status(400).json({
-    success: false,
-    message: "Invalid product id",
-  });
-}
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid product id",
+    });
+  }
   try {
     const categoryId = req.body.category;
     const category = await Category.findById(categoryId);
@@ -107,7 +135,11 @@ if(!mongoose.isValidObjectId(req.params.id)){
     }
 
     const productId = req.params.id;
-    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true });
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      req.body,
+      { new: true }
+    );
 
     if (updatedProduct) {
       res.status(200).json(updatedProduct);
@@ -150,7 +182,6 @@ router.delete("/:id", (req, res) => {
     });
 });
 
-
 router.get("/get/count", async (req, res) => {
   try {
     const productCount = await Product.countDocuments();
@@ -163,7 +194,7 @@ router.get("/get/count", async (req, res) => {
 
     res.json({
       success: true,
-      productCount: productCount
+      productCount: productCount,
     });
   } catch (error) {
     res.status(500).json({
@@ -173,8 +204,10 @@ router.get("/get/count", async (req, res) => {
   }
 });
 router.get(`/get/featured/:count`, async (req, res) => {
-  const count=req.params.count||0
-  const products = await Product.find({isFeatured: true}).select('name _id').limit(count);
+  const count = req.params.count || 0;
+  const products = await Product.find({ isFeatured: true })
+    .select("name _id")
+    .limit(count);
 
   if (!products) {
     res.status(500).json({ success: false });
